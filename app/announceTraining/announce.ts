8ded96f74230fd638e260a07148831140ed02cd9
gautammroads <gautamkumarmadhe@gmail.com>
2019-07-31 13:13:34 +0200

    2 files modified, 1 added

---
+++ app/announceTraining/announce.ts
@@ -0,0 +1,15 @@
+export interface Announce {
+
+id?:number;
+courseName?:string;
+venue?:string;
+trainerName:string
+nDuedate?:Date;
+nStartdate?:Date;
+nEnddate?:Date;
+courseContent?:string;
+contentFile?:File;
+
+
+
+}
--- app/announceTraining/register.component.ts
+++ app/announceTraining/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RegisterService } from './register.service';
+import { Announce } from './announce';
 
 @Component({
   selector: 'app-register',
@@ -8,9 +9,13 @@ import { RegisterService } from './register.service';
 })
 export class RegisterComponent implements OnInit {
 
+announce:Announce;
+
   constructor(private registerService:RegisterService) { }
 
+
   ngOnInit() {
+    this.announce.venue="pune";
   }
 
 }
